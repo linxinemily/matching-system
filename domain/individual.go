a48896b7b0803e605b63f9e0974f78f2d24ed58f
Emmie <tiahi5914@gmail.com>
2022-08-28 22:38:10 +0800

    validation

--- domain/individual.go
+++ domain/individual.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"matching-system/domain/enum"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -17,13 +18,43 @@ type Individual struct {
 
 func NewIndividual(id int, gender enum.Gender, age int, intro string, habits string, coord string) *Individual {
 	return &Individual{
-		id,
-		gender,
-		age,
-		intro,
-		habits,
-		coord,
+		id:     id,
+		gender: gender,
+		age:    validatedAge(age),
+		intro:  validatedIntro(intro),
+		habits: validatedHabits(habits),
+		coord:  validatedCoord(coord),
 	}
+}
+
+func validatedAge(age int) int {
+	if age <= 18 {
+		panic("age cannot under 18")
+	}
+	return age
+}
+
+func validatedIntro(intro string) string {
+	if len(intro) > 200 {
+		panic("intro cannot be more than 200 characters")
+	}
+	return intro
+}
+
+func validatedHabits(habits string) string {
+	re := regexp.MustCompile(`^[^\s,]+(?:,\s[^\s,]+)*$`)
+	if !re.Match([]byte(habits)) {
+		panic("invalid habits format, valid example: \"A, B, C\"")
+	}
+	return habits
+}
+
+func validatedCoord(coord string) string {
+	re := regexp.MustCompile(`\([0-9]*,[0-9]*\)`)
+	if !re.Match([]byte(coord)) {
+		panic("invalid coord format, valid example: \"(2, 4)\"")
+	}
+	return coord
 }
 
 func (i *Individual) GetXY() (x, y int) {
